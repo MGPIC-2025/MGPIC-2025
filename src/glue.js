5f3892ba911a6ee624a1e860bf6e063e947a55c1
东灯 <me@lampese.com>
2025-10-15 22:19:21 +0800

    feat: add default copper

--- src/glue.js
+++ src/glue.js
@@ -1,1 +1,17 @@
-import { core } from "./main.js";
+import {
+  get_global,
+  get_global_info,
+  global_get_resource,
+  global_get_copper_list,
+  global_gacha,
+  global_info_subscribe,
+} from "./main.js";
+
+export {
+  get_global,
+  get_global_info,
+  global_get_resource,
+  global_get_copper_list,
+  global_gacha,
+  global_info_subscribe,
+};
