b75024d5006caffe9e7b7afe4bdb379a43124031
东灯 <me@lampese.com>
2025-10-16 17:57:16 +0800

    feat: upgrade api & clean up api

--- src/glue.js
+++ src/glue.js
@@ -1,17 +1,33 @@
 import {
   get_global,
-  get_global_info,
-  global_get_resource,
-  global_get_copper_list,
-  global_gacha,
-  global_info_subscribe,
+  global_gacha as global_gacha_api,
+  global_get_resource as global_get_resource_api,
+  global_get_copper_list as global_get_copper_list_api,
+  global_upgrade_copper as global_upgrade_copper_api,
+  global_info_subscribe as global_info_subscribe_api,
 } from "./main.js";
 
-export {
-  get_global,
-  get_global_info,
-  global_get_resource,
-  global_get_copper_list,
-  global_gacha,
-  global_info_subscribe,
-};
+let global = get_global();
+let global_info = get_global_info();
+
+function gacha() {
+  return JSON.parse(global_gacha_api(global));
+}
+
+function get_resource() {
+  return JSON.parse(global_get_resource_api(global));
+}
+
+function get_copper_list() {
+  return JSON.parse(global_get_copper_list_api(global));
+}
+
+function upgrade_copper(id) {
+  return JSON.parse(global_upgrade_copper_api(global, id));
+}
+
+function info_subscribe(callback) {
+  global_info_subscribe_api(global_info, callback);
+}
+
+export { gacha, get_resource, get_copper_list, upgrade_copper, info_subscribe };
