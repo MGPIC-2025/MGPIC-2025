5be4a49dfac84cbe30f221158ecfc9c06fc186e9
东灯 <me@lampese.com>
2025-10-17 02:08:34 +0800

    fix: upgrade attribute issue

--- src/glue.js
+++ src/glue.js
@@ -1,6 +1,4 @@
 import {
-  get_global,
-  get_global_info,
   global_gacha as global_gacha_api,
   global_get_resource as global_get_resource_api,
   global_get_copper_list as global_get_copper_list_api,
@@ -8,27 +6,24 @@ import {
   global_info_subscribe as global_info_subscribe_api,
 } from "./main.js";
 
-let global = get_global();
-let global_info = get_global_info();
-
 function gacha() {
-  return JSON.parse(global_gacha_api(global));
+  return JSON.parse(global_gacha_api());
 }
 
 function get_resource() {
-  return JSON.parse(global_get_resource_api(global));
+  return JSON.parse(global_get_resource_api());
 }
 
 function get_copper_list() {
-  return JSON.parse(global_get_copper_list_api(global));
+  return JSON.parse(global_get_copper_list_api());
 }
 
 function upgrade_copper(id) {
-  return JSON.parse(global_upgrade_copper_api(global, id));
+  return JSON.parse(global_upgrade_copper_api(id));
 }
 
 function info_subscribe(callback) {
-  global_info_subscribe_api(global_info, callback);
+  global_info_subscribe_api((info) => callback(JSON.parse(info)));
 }
 
 export { gacha, get_resource, get_copper_list, upgrade_copper, info_subscribe };
