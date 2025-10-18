068b82c2d0ec99f61cb5a7caca773c972bc8794d
东灯 <me@lampese.com>
2025-10-19 07:04:11 +0800

    feat: frontend api

--- src/glue.js
+++ src/glue.js
@@ -4,6 +4,19 @@ import {
   global_get_copper_list as global_get_copper_list_api,
   global_upgrade_copper as global_upgrade_copper_api,
   global_info_subscribe as global_info_subscribe_api,
+  set_copper as set_copper_api,
+  set_enemy as set_enemy_api,
+  change_direction as change_direction_api,
+  move_to as move_to_api,
+  display_can_move as display_can_move_api,
+  display_can_attack as display_can_attack_api,
+  clear_state as clear_state_api,
+  animate_move as animate_move_api,
+  animate_reset as animate_reset_api,
+  put_map_block as put_map_block_api,
+  set_move_block as set_move_block_api,
+  set_attack_block as set_attack_block_api,
+  clear_block as clear_block_api,
 } from "./main.js";
 
 function gacha() {
